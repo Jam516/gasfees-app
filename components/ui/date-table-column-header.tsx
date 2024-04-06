129e32d85892e1c7f256a6e72cdaa4b07805c209
Jam516 <kufuorkofi@gmail.com>
2024-04-06 22:30:27 +0100

    commit

--- components/ui/date-table-column-header.tsx
+++ components/ui/date-table-column-header.tsx
@@ -55,7 +55,19 @@ export function DataTableColumnHeader<TData, TValue>({
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start">
-                    {/* ... */}
+                    <DropdownMenuItem onClick={() => column.toggleSorting(false)}>
+                        <ArrowUpIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                        Asc
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => column.toggleSorting(true)}>
+                        <ArrowDownIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                        Desc
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
+                        <EyeNoneIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+                        Hide
+                    </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </div>
